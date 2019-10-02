19da9d58f67323ba13b149862ef149ca19882476
Willisp <willispyoung@gmail.com>
2019-10-02 19:41:38 +0800

    learning

--- src/puppeteer/demo.js
+++ src/puppeteer/demo.js
@@ -4,10 +4,10 @@ puppeteer.launch().then(async browser => {
     const page = await browser.newPage();
 
     //var width = 1920, height = 1080;
-    //await page.setViewport({
-    //    width: width,
-    //    height: height
-    //});
+    await page.setViewport({
+        width: 600,
+        height: 1000
+    });
 
     //console.log(page.viewport().width, page.viewport().height);
 
@@ -15,28 +15,31 @@ puppeteer.launch().then(async browser => {
         const now = new Date().getTime();
         console.log(now);
 
-        try {
-            const allDivs = await page.$$('div');
-            var visibleCount = 0;
-            // Need to remove same/similar bBoxes!
-            for (var i = 0; i < allDivs.length; i++) {
-                var bbox = await allDivs[i].boundingBox();
-                if (bbox !== null) {
-                    visibleCount += 1;
-                    //console.log(bbox);
-                    console.log("X:", bbox.x, "Y:", bbox.y, "Width:", bbox.width, "Height:", bbox.height);
-                }
-            }
-            console.log("Visible Count:", visibleCount);
-        }
-        catch (e) {
+        await page.screenshot({ path: 'ss/' + now + ".png" });
+
+        //try {
+        //    const allDivs = await page.$$('div');
+        //    var visibleCount = 0;
+        //    // Need to remove same/similar bBoxes!
+        //    for (var i = 0; i < allDivs.length; i++) {
+        //        var bbox = await allDivs[i].boundingBox();
+        //        if (bbox !== null) {
+        //            visibleCount += 1;
+        //            //console.log(bbox);
+        //            console.log("X:", bbox.x, "Y:", bbox.y,
+        //                "Width:", bbox.width, "Height:", bbox.height);
+        //        }
+        //    }
+        //    console.log("Visible Count:", visibleCount);
+        //}
+        //catch (e) {
             
-        }
-    }, 100);
+        //}
+    }, 80);    // This interval is important.
 
     page.on("domcontentloaded", async function () {
         console.log('domContentLoaded');
-        page.screenshot({ path: 'dcl.png' });
+        await page.screenshot({ path: 'dcl.png' });
     });
 
     page.on('load', async function () {
@@ -47,7 +50,7 @@ puppeteer.launch().then(async browser => {
         await browser.close();
     });
 
-    await page.goto('https://www.baidu.com');
+    await page.goto('https://www.zhihu.com/topic/19565870/');
 
     var input = process.stdin.read();
 });
