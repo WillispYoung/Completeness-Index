489863189a2a63fa5735e5716ba968e5327be9b0
willisp <willisp@gmail.com>
2019-10-04 16:00:59 +0800

    daily test

--- src/puppeteer/demo.js
+++ src/puppeteer/demo.js
@@ -15,27 +15,27 @@ puppeteer.launch().then(async browser => {
         const now = new Date().getTime();
         console.log(now);
 
-        await page.screenshot({ path: 'ss/' + now + ".png" });
+        // await page.screenshot({ path: 'ss/' + now + ".png" });
 
-        //try {
-        //    const allDivs = await page.$$('div');
-        //    var visibleCount = 0;
-        //    // Need to remove same/similar bBoxes!
-        //    for (var i = 0; i < allDivs.length; i++) {
-        //        var bbox = await allDivs[i].boundingBox();
-        //        if (bbox !== null) {
-        //            visibleCount += 1;
-        //            //console.log(bbox);
-        //            console.log("X:", bbox.x, "Y:", bbox.y,
-        //                "Width:", bbox.width, "Height:", bbox.height);
-        //        }
-        //    }
-        //    console.log("Visible Count:", visibleCount);
-        //}
-        //catch (e) {
+        try {
+           const allDivs = await page.$$('div');
+           var visibleCount = 0;
+           // Need to remove same/similar bBoxes!
+           for (var i = 0; i < allDivs.length; i++) {
+               var bbox = await allDivs[i].boundingBox();
+               if (bbox !== null) {
+                   visibleCount += 1;
+                   //console.log(bbox);
+                   console.log("X:", bbox.x, "Y:", bbox.y,
+                       "Width:", bbox.width, "Height:", bbox.height);
+               }
+           }
+           console.log("Visible Count:", visibleCount);
+        }
+        catch (e) {
             
-        //}
-    }, 80);    // This interval is important.
+        }
+    }, 100);    // This interval is important.
 
     page.on("domcontentloaded", async function () {
         console.log('domContentLoaded');
