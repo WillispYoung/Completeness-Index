848f3b6037d82ce2eaa571ba4a4f5754a12c3426
willisp <willisp@gmail.com>
2019-10-11 14:15:25 +0800

    utilizing layer tree

--- src/puppeteer/demo.js
+++ src/puppeteer/demo.js
@@ -12,65 +12,65 @@ puppeteer.launch().then(async browser => {
     //console.log(page.viewport().width, page.viewport().height);
 
     // ---> Record layer change and other events periodically.
-    var interval = setInterval(async function() {
-        const now = new Date().getTime();
-        // console.log(now);
+    // var interval = setInterval(async function() {
+    //     const now = new Date().getTime();
+    //     // console.log(now);
 
-        // await page.screenshot({ path: 'ss/' + now + ".png" });
+    //     // await page.screenshot({ path: 'ss/' + now + ".png" });
 
-        try {
-            // Remove same DIVs.
-            const allDivs = await page.$$('div');
-            // console.log("Number of DIVs:", allDivs.length);
+    //     try {
+    //         // Remove same DIVs.
+    //         const allDivs = await page.$$('div');
+    //         // console.log("Number of DIVs:", allDivs.length);
 
-            const visibleDivs = [];
-            // var visibleCount = 0;
-// 
-            for (var i = 0; i < allDivs.length; i++) {
-                var bbox = await allDivs[i].boundingBox();
-                if (bbox != null) {
-                    if (visibleDivs.length == 0) {
-                        visibleDivs.push(bbox);
-                        continue;
-                    }
+    //         const visibleDivs = [];
+    //         // var visibleCount = 0;
 
-                    for (var j = 0; j < visibleDivs.length; j++) {
-                        if (bbox.x == visibleDivs[j].x &&
-                            bbox.y == visibleDivs[j].y &&
-                            bbox.width == visibleDivs[j].width &&
-                            bbox.height == visibleDivs[j].height)
-                            continue;
-                        else {
-                            visibleDivs.push(bbox);
-                            break;
-                        }
-                    }
-                }
-                // if (bbox !== null) {
-                //     visibleCount += 1;
-                //     //console.log(bbox);
-                //     console.log("X:", bbox.x, "Y:", bbox.y,
-                //         "Width:", bbox.width, "Height:", bbox.height);
-                // }
-            }
+    //         for (var i = 0; i < allDivs.length; i++) {
+    //             var bbox = await allDivs[i].boundingBox();
+    //             if (bbox != null) {
+    //                 if (visibleDivs.length == 0) {
+    //                     visibleDivs.push(bbox);
+    //                     continue;
+    //                 }
 
-            // console.log("Visible Count:", visibleDivs.length);
-        } catch (e) {
-            // console.log(e);
-        }
-    }, 100); // This interval is important.
+    //                 for (var j = 0; j < visibleDivs.length; j++) {
+    //                     if (bbox.x == visibleDivs[j].x &&
+    //                         bbox.y == visibleDivs[j].y &&
+    //                         bbox.width == visibleDivs[j].width &&
+    //                         bbox.height == visibleDivs[j].height)
+    //                         continue;
+    //                     else {
+    //                         visibleDivs.push(bbox);
+    //                         break;
+    //                     }
+    //                 }
+    //             }
+    //             // if (bbox !== null) {
+    //             //     visibleCount += 1;
+    //             //     //console.log(bbox);
+    //             //     console.log("X:", bbox.x, "Y:", bbox.y,
+    //             //         "Width:", bbox.width, "Height:", bbox.height);
+    //             // }
+    //         }
+
+    //         // console.log("Visible Count:", visibleDivs.length);
+    //     } catch (e) {
+    //         // console.log(e);
+    //     }
+    // }, 100); // This interval is important.
 
     page.on("domcontentloaded", async function() {
-        console.log('domContentLoaded');
+        console.log('DOM Content Loaded');
         // await page.screenshot({ path: 'dcl.png' });
     });
 
     page.on('load', async function() {
         console.log('Page Loaded');
-        clearInterval(interval);
+        // clearInterval(interval);
 
         // await page.screenshot({ path: 'l.png' });
-        // await browser.close();
+        // await browser.close();	// Don't close now!
     });
 
     // ---> Combine CDP together.
@@ -81,17 +81,33 @@ puppeteer.launch().then(async browser => {
         client.on("Network.responseReceived", async function(requestId) {
             try {
                 var response = await client.send('Network.getResponseBody', requestId);
-                console.log(response.base64Encoded);
-                if (!response.base64Encoded) {	// --> Contains JavaScript files.
-                	console.log(response.body);
-            	}
+                // console.log(response.base64Encoded);
+                if (!response.base64Encoded) {
+                    // --> Contains JavaScript files.
+                    // console.log(response.body);
+                    var content = response.body;
+
+                }
             } catch (e) {
-            	// console.log(e);
-            	console.log("Error receiving response.");
+                // console.log(e);
+                console.log("Error receiving response.");
             }
         });
+
+        await client.send('LayerTree.enable');
+
+        client.on('LayerTree.layerPainted', async function(layerId, clip) {
+            try {
+                var reasons = await client.send('LayerTree.compositingReasons', layerId);
+                console.log(clip);
+                console.log(reasons);
+            } catch (e) {
+                console.log("Error capturing layer paint events.");
+            }
+        });
+
     } catch (e) {
-    	console.log(e);
+        // console.log(e);
         console.log("Error creating CDP session.");
     }
 
