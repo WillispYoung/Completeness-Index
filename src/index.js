16e5d3987210fa8d3b7146beb6051cf9033658b1
willisp <willisp@gmail.com>
2019-11-04 16:35:59 +0800

    learning CDP

--- src/index.js
+++ src/index.js
@@ -25,18 +25,29 @@ emitter.on('traceEnd', () => {
 
             events.forEach(e => {
                 if (cat_name[e.cat]) {
-                    cat_name[e.cat].push(e.name);
+                    if (cat_name[e.cat][e.name])
+                        cat_name[e.cat][e.name] ++;
+                    else
+                        cat_name[e.cat][e.name] = 1;
                 }
                 else {
-                    cat_name[e.cat] = [e.name];
+                    cat_name[e.cat] = {};
+                    cat_name[e.cat][e.name] = 1;
                 }
             });
 
-            Object.keys(cat_name).forEach(key => {
-                cat_name[key] = Array.from(new Set(cat_name[key]));
-            });
+            // Object.keys(cat_name).forEach(key => {
+            //     cat_name[key] = Array.from(new Set(cat_name[key]));
+            // });
 
-            fs.writeFileSync(trace_path.replace('trace', 'category'), JSON.stringify(cat_name));
+            output = JSON.stringify(cat_name);
+            // while (output.includes('[')) {
+            //     output = output.replace('[', '{');
+            // }
+            // while (output.includes(']')) {
+            //     output = output.replace(']', '}');
+            // }
+            fs.writeFileSync(trace_path.replace('trace', 'category'), output);
         }
     }, 1000);
 });
@@ -47,18 +58,24 @@ puppeteer.launch().then(async browser => {
         now = now.replace(':', '-');
     }
     now.replace('\.', '-');
+    
+    final_url = url;
+    while (url.includes(':') || url.includes("/")) {
+        url = url.replace(':', '');
+        url = url.replace('/', '');
+    }
 
     if (url.startsWith("https"))
-        trace_path = `trace-${url.substring(8)}-${now}.json`;
+        trace_path = `trace-${url.substring(6)}-${now}.json`;
     else if (url.startsWith("http"))
-        trace_path = `trace-${url.substring(7)}-${now}.json`;
+        trace_path = `trace-${url.substring(5)}-${now}.json`;
 
     page = await browser.newPage();
     await page.tracing.start({
         path: trace_path,
         screenshot: true
     });
-    await page.goto(url);
+    await page.goto(final_url);
     await page.tracing.stop().then(() => {
         browser.close();
         emitter.emit('traceEnd');
