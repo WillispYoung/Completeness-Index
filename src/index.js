9881839162b3537acdc16028d57f93c463c896ce
willisp <willisp@gmail.com>
2019-11-04 15:47:57 +0800

    study tracing

--- src/index.js
+++ src/index.js
@@ -1,25 +1,66 @@
-var fs = require("fs");
-var trace_file = "trace2019-10-25T09-1.json"
+const EventEmitter = require('events');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
 
-var content = JSON.parse(fs.readFileSync(trace_file));
-var events = content.traceEvents;
+let url, trace_path;
 
-var categories = new Set();
-var names = new Set();
-var category_name = new Set();
+if (process.argv.length < 3) {
+    url = "about:blank";
+}
+else {
+    url = process.argv[2];
+}
 
-events.forEach(event => {
-	event.cat.split(',').forEach(s => categories.add(s));
-	names.add(event.name);
-	category_name.add(`${event.cat}  ${event.name}`);
+const emitter = new EventEmitter();
+
+emitter.on('traceEnd', () => {
+    console.log("Tracing finished.");
+
+    setTimeout(() => {
+        if (fs.existsSync(trace_path)) {
+            var content = JSON.parse(fs.readFileSync(trace_path));
+            var events = content.traceEvents;
+
+            var cat_name = {};
+
+            events.forEach(e => {
+                if (cat_name[e.cat]) {
+                    cat_name[e.cat].push(e.name);
+                }
+                else {
+                    cat_name[e.cat] = [e.name];
+                }
+            });
+
+            Object.keys(cat_name).forEach(key => {
+                cat_name[key] = Array.from(new Set(cat_name[key]));
+            });
+
+            fs.writeFileSync(trace_path.replace('trace', 'category'), JSON.stringify(cat_name));
+        }
+    }, 1000);
 });
 
-categories = Array.from(categories);
-categories.sort();
-console.log("Category:", categories);
+puppeteer.launch().then(async browser => {
+    now = new Date().toISOString();
+    while (now.includes(':')) {
+        now = now.replace(':', '-');
+    }
+    now.replace('\.', '-');
 
-// console.log("Name:", names);
+    if (url.startsWith("https"))
+        trace_path = `trace-${url.substring(8)}-${now}.json`;
+    else if (url.startsWith("http"))
+        trace_path = `trace-${url.substring(7)}-${now}.json`;
 
-category_name = Array.from(category_name);
-category_name.sort();
-category_name.forEach(d => console.log(d));
+    page = await browser.newPage();
+    await page.tracing.start({
+        path: trace_path,
+        screenshot: true
+    });
+    await page.goto(url);
+    await page.tracing.stop().then(() => {
+        browser.close();
+        emitter.emit('traceEnd');
+    });
+});
--- src/puppeteer/get_trace.js
+++ src/puppeteer/get_trace.js
@@ -4,7 +4,11 @@ puppeteer.launch().then(async browser => {
     now = new Date().toISOString().replace(':', '-').substring(0, 15);
 
     page = await browser.newPage();
-    await page.tracing.start({path: `trace${now}.json`});
+    await page.tracing.start({
+        path: `trace${now}.json`,
+        screenshot: true,
+        categories: ['devtools.timeline','disabled-by-default-devtools.timeline']
+    });
     await page.goto('https://www.baidu.com');
     await page.tracing.stop().then(() => {
         browser.close();
